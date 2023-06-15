ae937d39d0d757cba26d9eb0662940485c193b97
Guilherme <guigasthepro@gmail.com>
2023-06-15 16:40:23 +0100

    Adding allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cka=@9vi@u@m!d$3uefik&9&0-q%7(sn9iap#53)g@md-4ol+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'oguigas.pythonanywhere.com']
 
 
 # Application definition
